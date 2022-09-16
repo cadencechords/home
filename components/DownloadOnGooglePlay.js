fcf6930816cd2e5eb3268ee9e00d244828e3b5a3
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-09-15 21:37:49 -0400

    Added button (no link yet) to Google Play

---
+++ components/DownloadOnGooglePlay.js
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import GooglePlayImage from '../public/googleplay.svg';
+
+export default function DownloadOnGooglePlayButton() {
+  return (
+    <Link href="">
+      <a className="flex items-center justify-center w-full h-full px-4 py-3 bg-black rounded-lg md:py-2 md:w-40">
+        <Image src={GooglePlayImage} className="w-full" />
+      </a>
+    </Link>
+  );
+}
--- components/Footer.js
+++ components/Footer.js
@@ -2,16 +2,24 @@ import Image from 'next/image';
 import React from 'react';
 import LogoImage from '../public/icon.png';
 import DownloadOnAppStoreButton from './DownloadOnAppStoreButton';
+import DownloadOnGooglePlayButton from './DownloadOnGooglePlay';
 
 export default function Footer() {
   return (
-    <div className="w-full my-8">
+    <div className="w-full mt-8 mb-32 sm:my-8">
       <div className="flex justify-center w-full">
         <div className="flex flex-col items-center w-full">
-          <div className="flex items-center justify-center w-16 mb-4">
+          <div className="flex items-center justify-center w-16 mb-10">
             <Image src={LogoImage} alt="Cadence icon" height={128} width={96} />
           </div>
-          <DownloadOnAppStoreButton />
+          <div className="flex flex-col w-full sm:w-auto sm:flex-row">
+            <div className="w-full mb-1 sm:mr-2 sm:mb-0">
+              <DownloadOnAppStoreButton />
+            </div>
+            <div className="w-full mt-1 sm:ml-2 sm:mt-0">
+              <DownloadOnGooglePlayButton />
+            </div>
+          </div>
         </div>
       </div>
     </div>
