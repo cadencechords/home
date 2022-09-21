d00b9a61ca88b5a645bb73d90272f09ab8b85b31
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-09-21 15:20:30 -0400

    Added Google Play link

--- components/DownloadOnGooglePlay.js
+++ components/DownloadOnGooglePlay.js
@@ -5,7 +5,7 @@ import GooglePlayImage from '../public/googleplay.svg';
 
 export default function DownloadOnGooglePlayButton() {
   return (
-    <Link href="">
+    <Link href="https://play.google.com/store/apps/details?id=com.cadencechords">
       <a className="flex items-center justify-center w-full h-full px-4 py-3 bg-black rounded-lg md:py-2 md:w-40">
         <Image src={GooglePlayImage} className="w-full" />
       </a>
