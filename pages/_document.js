2c7b30d52b7f887e1d7cf86816051c76ea8ec810
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-09-27 15:23:59 -0400

    Tag update

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,13 @@ export default class MyDocument extends Document {
                             `,
             }}
           />
+          <script
+            src={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NCJKTB5');`}
+          />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -33,6 +40,14 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NCJKTB5"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
