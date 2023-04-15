33a5ed74008bffdf39a8445f7bb6dcb051f79a50
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-04-15 16:43:38 -0400

    add facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,31 @@
-import "tailwindcss/tailwind.css";
-import "../styles/styles.css";
+import 'tailwindcss/tailwind.css';
+import '../styles/styles.css';
+import { useEffect } from 'react';
+import { Router } from 'next/router';
+
+function FacebookPixel() {
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then(x => x.default)
+      .then(ReactPixel => {
+        ReactPixel.init('240227321861121');
+        ReactPixel.pageView();
+
+        Router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView();
+        });
+      });
+  });
+  return null;
+}
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return (
+    <>
+      <FacebookPixel />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
